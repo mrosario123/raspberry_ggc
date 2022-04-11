8b26036a181ce2417bf57d529dd15e4f5d1f1c77
mrosario123 <54247576+mrosario123@users.noreply.github.com>
2022-04-10 20:24:04 -0400

    Added files via upload

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+
+// image file size
+app.use(bodyParser.json({ limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true}))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// connecting MongoDB to server
+//const CONNECTION_URL = 'mongodb+srv://matthew_pi:pi_project@cluster0.i7itx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 4000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    // if connection is successful
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`)))
+    // if connection is not successful
+    .catch((error) => console.log(error.message));
+
+// makes sure we don't get any warnings in the console
+// mongoose.set('useFindAndModify', false);
