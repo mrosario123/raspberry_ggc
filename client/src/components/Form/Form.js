eeab26eee6275e4e7a75a3e89456e5010a2c40d4
mrosario123 <mrosario1@ggc.edu>
2022-04-24 14:50:43 -0400

    Got rid of unnecessary code

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -6,7 +6,7 @@ import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
-  const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+  const [postData, setPostData] = useState({title: '', message: '', selectedFile: '' });
   const post = useSelector((state) => (currentId ? state.posts.find((message) => message._id === currentId) : null));
   const dispatch = useDispatch();
   const classes = useStyles();
@@ -17,7 +17,7 @@ const Form = ({ currentId, setCurrentId }) => {
 
   const clear = () => {
     setCurrentId(0);
-    setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+    setPostData({title: '', message: '', selectedFile: '' });
   };
 
   const handleSubmit = async (e) => {
